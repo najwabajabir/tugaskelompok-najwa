3628bb9b0bc40571788dc7586dfde8fdb6c457ec
najwa 81 <najwabajabir@gmail.com>
2019-12-12 19:46:41 +0700

    tugas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper gambar">
+      <div className="wrapper">
+        <div className="flex-judul">
+          <div className="judul">
+            <h1 style={{ color: 'white' }}>Sekolah</h1>
+            <h1 class="unggul">Unggul</h1>
+          </div>
+        </div>
+        <div className="wrap-description">
+          <div className="description">
+            <p><b>Lorem Ipsum</b> is simply dummy text of the printing<br /> and typesetting industry. Lorem Ipsum has been the <br />industry's standard dummy text ever since the 1500s,<br /> when an unknown printer took a galley of type and <br />scrambled it to make a type specimen book. </p>
+          </div>
+        </div>
+        <div className="top-button">
+          <div className="wrap-button">
+            <div className="button">
+              Baca Selengkapnya
+        </div>
+          </div>
+
+          <div className="right">
+            <i class="fa fa-chevron-left"></i>
+          </div>
+          <div className="left">
+            <i class="fa fa-chevron-right"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
